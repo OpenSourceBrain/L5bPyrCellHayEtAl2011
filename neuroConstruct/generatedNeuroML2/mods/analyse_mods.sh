23c0c1f5a86b78b7eca9d0f42afdfd8783bfb740
Padraig Gleeson <p.gleeson@gmail.com>
2015-06-08 19:17:14 +0100

    More on comparison

--- neuroConstruct/generatedNeuroML2/mods/analyse_mods.sh
+++ neuroConstruct/generatedNeuroML2/mods/analyse_mods.sh
@@ -1,2 +1,13 @@
-pynml-modchananalysis NaTa_t -stepV 5 -temperature 6.3
+# CaDynamics_E2.mod Ca_HVA.mod Ca_LVAst.mod Ih.mod Im.mod K_Pst.mod K_Tst.mod NaTa_t.mod NaTs2_t.mod Nap_Et2.mod SK_E2.mod SKv3_1.mod
 
+pynml-modchananalysis NaTa_t -stepV 5 -temperature 6.3
+pynml-modchananalysis Nap_Et2 -stepV 5 -temperature 6.3
+pynml-modchananalysis K_Pst -stepV 5 -temperature 6.3
+pynml-modchananalysis K_Tst -stepV 5 -temperature 6.3
+pynml-modchananalysis Ca_HVA -stepV 5 -temperature 6.3
+pynml-modchananalysis Ca_LVAst -stepV 5 -temperature 6.3
+pynml-modchananalysis Ih -stepV 5 -temperature 6.3
+pynml-modchananalysis SKv3_1 -stepV 5 -temperature 6.3
+
+pynml-modchananalysis SK_E2 -stepV 5 -temperature 6.3
+pynml-modchananalysis Im -stepV 5 -temperature 6.3
