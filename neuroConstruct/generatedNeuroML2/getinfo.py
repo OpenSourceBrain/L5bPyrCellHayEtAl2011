066465b29dce53e92de546b32b92b0aef8bc599e
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-09-01 11:26:13 +0100

    chore: clean up

--- neuroConstruct/generatedNeuroML2/getinfo.py
+++ neuroConstruct/generatedNeuroML2/getinfo.py
@@ -28,16 +28,4 @@ with open("NeuroML-morphinfo.yaml", "w") as f:
     print(yaml.dump(retval, sort_keys=True, indent=4), file=f, flush=True)
 with open("NeuroML-info.yaml", "w") as f:
     retval = getinfo(h.allsec())
-    # manually make some tweaks to ease comparison
-    # included as CaDynamics_E2_NML
-    """
-    del retval['mechanisms']['CaDynamics_E2']
-    dicta = retval['mechanisms']['CaDynamics_E2_NML2']['parameters']
-    # replace parameters suffixed by _NML2
-    for key, value in dicta.items():
-        if "_NML2" in key:
-            new_key = key[:-5]
-            dicta[new_key] = dicta.pop(key)
-    """
-
     print(yaml.dump(retval, sort_keys=True, indent=4), file=f, flush=True)
